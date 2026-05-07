30e713f7b49ef59b50269594fa255b4dd36075f9
dario <dariomonrabal@gmail.com>
2026-05-07 16:14:47 +0200

    holita

--- src/recetas/Recetas.java
+++ src/recetas/Recetas.java
@@ -18,7 +18,7 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+//holita
 class ConnectionSingleton {
 	private static Connection con;
 
